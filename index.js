c6cb4421dfe7b5ce03cfdfecbff4c9e5a16dac07
Hassan Bouzlima <bouzlima.h@live.fr>
2020-02-15 21:51:35 +0100

    another commit

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const path = require('path');
-
+ 
 const app = express();
+ 
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/book-search'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/book-search/index.html'));
+app.use(express.static(__dirname + '/dist/book-search'));
+ 
+app.get('*', function(req,res) {
+  res.sendFile(path.join(__dirname + '/dist/book-search/index.html'));
 });
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
